766a8be28e93f9b2268f295e9d9ab44197d7093e
Maicol Montoya <maicolmontoya323@gmail.com>
2025-05-15 01:50:51 -0500

    initialize project structure with Maven and Spring Boot setup

---
+++ src/main/java/cue/edu/co/moduloaulas/ModuloAulasApplication.java
@@ -0,0 +1,13 @@
+package cue.edu.co.moduloaulas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloAulasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloAulasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cue/edu/co/moduloaulas/ModuloAulasApplicationTests.java
@@ -0,0 +1,13 @@
+package cue.edu.co.moduloaulas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloAulasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
